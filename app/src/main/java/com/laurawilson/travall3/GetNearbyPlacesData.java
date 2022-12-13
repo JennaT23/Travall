736052cea3a0a535e3dee58837ba34c1a2e82062
mrswi <ljw5533@psu.edu>
2022-12-12 20:45:45 -0500

    3 classes for the google maps, DataParser, DownloadURL and GetNearbyPlacesData

---
+++ app/src/main/java/com/laurawilson/travall3/GetNearbyPlacesData.java
@@ -0,0 +1,66 @@
+package com.laurawilson.travall3;
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {
+
+    String googlePlacesData;
+    GoogleMap mMap;
+    String url;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+
+        DownloadURL downloadURL = new DownloadURL();
+        try {
+            googlePlacesData = downloadURL.readURL(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return googlePlacesData;
+    }
+
+    public void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList) {
+        for (int i =0; i < nearbyPlaceList.size(); i++)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String, String> googlePLace = nearbyPlaceList.get(i);
+
+            String placeName = googlePLace.get("place_name");
+            String vicinity = googlePLace.get("vicinity");
+            double lat = Double.parseDouble(Objects.requireNonNull(googlePLace.get("lat")));
+            double lng = Double.parseDouble(Objects.requireNonNull(googlePLace.get("lng")));
+
+            LatLng latLng = new LatLng(lat, lng);
+
+            markerOptions.position(latLng);
+            markerOptions.title(placeName + " " + vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+            //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        }
+    }
+    @Override
+    protected void onPostExecute(String s) {
+        List<HashMap<String, String>> nearbyPlaceList = null;
+        DataParser parser = new DataParser();
+
+        nearbyPlaceList = parser.parse(s);
+        showNearbyPlaces(nearbyPlaceList);
+    }
+}
